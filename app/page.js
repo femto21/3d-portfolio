dcc8d3160fa6710d0c66cdf370c2de09b6f62c26
Arpit-Gahlot <ar700601@dal.ca>
2024-12-28 01:08:05 -0400

    commit 4

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./_sections/Navbar";
 import Hero from "./_sections/Hero";
+import About from "./_sections/About";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 }
